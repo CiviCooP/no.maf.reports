49c89572c2dec7eb6cf3b5ab428fa461756a6ffe
Jaap Jansma <info@edeveloper.nl>
2014-11-25 19:47:38 +0100

    update added details text to activity report

---
+++ CRM/Reports/Form/Report/Activity.php
@@ -0,0 +1,51 @@
+<?php
+// $Id$
+
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 4.3                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2013                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License and the CiviCRM Licensing Exception along                  |
+ | with this program; if not, contact CiviCRM LLC                     |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC (c) 2004-2013
+ * $Id$
+ *
+ */
+class CRM_Reports_Form_Report_Activity extends CRM_Report_Form_Activity {
+  
+  function __construct() {
+    parent::__construct();
+
+    $this->_columns['civicrm_activity']['fields']['details'] = array(
+      'title' => ts('Details'),
+    );
+    $this->_columns['civicrm_activity']['filters']['details'] = array(
+      'title' => ts('Details'),
+      'type' => CRM_Utils_Type::T_TEXT,
+    );
+  }
+}
+
